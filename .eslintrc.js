0fa1651423d0015864a5ca4523b63dd646ac4604
nekocode <nekocode.cn@gmail.com>
2020-06-10 14:25:24 +0800

    Replace tslint with eslint & update deps

---
+++ .eslintrc.js
@@ -0,0 +1,18 @@
+module.exports = {
+  root: true,
+  parser: '@typescript-eslint/parser',
+  plugins: ['@typescript-eslint'],
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/eslint-recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:react/recommended',
+    'plugin:react-hooks/recommended',
+    'plugin:prettier/recommended',
+    'prettier/react',
+    'prettier/standard',
+  ],
+  rules: {
+    'prettier/prettier': 'error',
+  },
+};
