52e2d40bf82188229e9b424e832dcc4916e97ff9
nekocode <nekocode.cn@gmail.com>
2021-05-10 15:54:14 +0800

    refactor with mutable source

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,23 @@
-import React, { useState, useRef, useCallback } from 'react';
-import { ValueNotifier, useListen } from './listenable';
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import React, { useRef, useCallback } from 'react';
+import { ValueNotifier } from './listenable';
 export * from './listenable';
 
-export class SharedState<T> extends ValueNotifier<T> {}
+const createMutableSource = (React as any).unstable_createMutableSource;
+const useMutableSource = (React as any).unstable_useMutableSource;
+
+export class SharedState<T> extends ValueNotifier<T> {
+  public source: any;
+
+  public constructor(protected value: T) {
+    super(value);
+    this.source = createMutableSource(this, () => this.getValue());
+  }
+}
+
+function getSnapshot<T>(sharedState: SharedState<T>) {
+  return sharedState.getValue();
+}
 
 /**
  * Hook a shared state to component
@@ -14,26 +29,29 @@ export function useSharedState<T>(
   sharedState: SharedState<T>,
   shouldUpdate: boolean | ((current: T, prev: T) => boolean) = true,
 ): [T, React.Dispatch<React.SetStateAction<T>>] {
-  const [, setState] = useState<T>(sharedState.getValue());
-  const shouldUpdateRef = useRef<
-    boolean | ((current: T, prev: T) => boolean)
-  >();
+  const shouldUpdateRef =
+    useRef<boolean | ((current: T, prev: T) => boolean)>();
   shouldUpdateRef.current = shouldUpdate;
 
-  const listener = useCallback(
-    (current: T, prev: T) => {
-      const f = shouldUpdateRef.current;
-      if (f === false || (f instanceof Function && !f(current, prev))) {
-        // If the `shouldUpdate` is or returns false, do not update state
-        return;
-      }
+  const subscribe = useCallback(
+    (sharedState: SharedState<T>, callback: () => void) => {
+      const listener = (current: T, prev: T) => {
+        const f = shouldUpdateRef.current;
+        if (f === false || (f instanceof Function && !f(current, prev))) {
+          // If the `shouldUpdate` is or returns false, do not update state
+          return;
+        }
 
-      setState(current);
+        callback();
+      };
+
+      sharedState.addListener(listener);
+      return () => sharedState.removeListener(listener);
     },
-    [setState],
+    [shouldUpdateRef],
   );
 
-  useListen(sharedState, listener);
+  const state: T = useMutableSource(sharedState.source, getSnapshot, subscribe);
 
   // return the same function object between renderings
   const setSharedState = useCallback(
@@ -43,5 +61,5 @@ export function useSharedState<T>(
     [sharedState],
   );
 
-  return [sharedState.getValue(), setSharedState];
+  return [state, setSharedState];
 }
